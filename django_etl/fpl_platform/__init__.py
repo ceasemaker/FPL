142953dcd307aeebc2f0f884da322579975eb5f8
ceasemaker <ceasemaker@gmail.com>
2025-10-07 01:21:30 +0200

    Fix: Make celery import optional during build process

--- django_etl/fpl_platform/__init__.py
+++ django_etl/fpl_platform/__init__.py
@@ -6,6 +6,9 @@ Loads Celery app on Django startup for task scheduling.
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from .celery import app as celery_app
-
-__all__ = ('celery_app',)
+try:
+    from .celery import app as celery_app
+    __all__ = ('celery_app',)
+except ImportError:
+    # Celery not installed yet (during build process)
+    pass
